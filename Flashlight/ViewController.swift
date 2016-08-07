3c1bd7790349cd51ce5d583becfeedc7b4a04d31
thefreshduke <scottyshaw1@gmail.com>
2016-08-06 17:28:51 -0700

    Changed code for flashlight functionality

--- Flashlight/ViewController.swift
+++ Flashlight/ViewController.swift
@@ -11,44 +11,63 @@ import AVFoundation
 
 class ViewController: UIViewController {
     
-    //    var myButtom: UIButton!
     let myDevice = AVCaptureDevice.defaultDeviceWithMediaType(AVMediaTypeVideo)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        let myButton = UIButton(frame: CGRect(x: self.view.bounds.width / 2, y: self.view.bounds.height / 2, width: 250, height: 125))
+        let myButton = UIButton(frame: CGRect(x: self.view.bounds.width / 2 - 125, y: self.view.bounds.height / 2 - 62.5, width: 250, height: 125))
         myButton.backgroundColor = UIColor.blueColor()
         myButton.setTitle("ON", forState: UIControlState.Normal)
         myButton.addTarget(self, action: #selector(buttonAction), forControlEvents: UIControlEvents.TouchUpInside)
-        
         self.view.addSubview(myButton)
     }
     
     func buttonAction(sender: UIButton!) {
-        if (myDevice.hasTorch) {
+        
+        //        let avDevice = AVCaptureDevice.defaultDeviceWithMediaType(AVMediaTypeVideo)
+        // check if the device has torch
+        if  myDevice.hasTorch {
+            // lock your device for configuration
             do {
                 try myDevice.lockForConfiguration()
-            } catch {
+            }
+            catch {
                 return
             }
+            
+            // check if your torchMode is on or off. If on turns it off otherwise turns it on
+            //            myDevice.torchMode = myDevice.torchActive ? AVCaptureTorchMode.Off : AVCaptureTorchMode.On
+            
+            //            if (sender.currentTitle == "ON") {
+            //                myDevice.torchMode = AVCaptureTorchMode.Off
+            //                sender.setTitle("ON", forState: UIControlState.Normal);
+            //            }
+            //            else {
+            //                myDevice.torchMode = AVCaptureTorchMode.On
+            //                sender.setTitle("OFF", forState: UIControlState.Normal);
+            //            }
+            
+            // sets the torch intensity to 100%
+            //            if (!self.myDevice.torchActive) {
+            
             if (sender.currentTitle == "ON") {
-                myDevice.torchMode = AVCaptureTorchMode.Off
-                sender.setTitle("ON", forState: UIControlState.Normal);
-            }
-            else {
-                myDevice.torchMode = AVCaptureTorchMode.On
+                do {
+                    try self.myDevice.setTorchModeOnWithLevel(1.0)
+                }
+                catch {
+                    return
+                }
                 sender.setTitle("OFF", forState: UIControlState.Normal);
             }
+            else {
+                self.myDevice.torchMode = AVCaptureTorchMode.Off
+                sender.setTitle("ON", forState: UIControlState.Normal);
+            }
+            
+            // unlock your device
             myDevice.unlockForConfiguration()
-        }
-        else {
-            //            let alertView = UIAlertControllerStyleAlert()
-            //            alertView.message = "No Flash Detected!"
-            //            alertView.addButtonWithTitle("OK")
-            //            alertView.show()
-            print("no flash")
         }
     }
     
